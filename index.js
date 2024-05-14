48e1b6fd0916f496eb05673ccd59a8b860813cf1
sophia-asahi <sophia@mail.asahitechnologies.com>
2024-05-14 19:33:00 +0530

    files added

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $('#year').text(new Date().getFullYear());
+});
+$(document).ready(function(){
+$("#success-form").hide();
+});
+// $(document).ready(function(){
+//     $("#submitBtn").click(function(){
+//         $("#demo").hide();
+//       $("#success-form").show();
+//     });})
+
+
